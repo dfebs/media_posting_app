a8e622249ffeba6dfd141457c86d2fba4bded42b
dfebs <daniel.febles97@gmail.com>
2023-07-08 15:03:17 -0400

    added models and updated dependencies

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    image = models.ImageField()
+    text = models.TextField(max_length=50)
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    post = models.ForeignKey(Post)
+    text = models.TextField(max_length=25)
+
+class Reaction: # This should only be edited by admins
+    image = models.ImageField()
+
+class PostReaction:
+    reactor = models.ForeignKey(settings.AUTH_USER_MODEL)
+    post = models.ForeignKey(Post)
+    reaction = models.ForeignKey(Reaction)
