37bfb4fbc4802c1830eb73c8f506ff642956aa80
dfebs <daniel.febles97@gmail.com>
2023-08-27 13:27:59 -0400

    reorganized settings.py to account for different environments

--- media_posting_app/settings/defaults.py
+++ media_posting_app/settings/defaults.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 LOGIN_REDIRECT_URL = '/'
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-y6+#7zhizg0e+y!97qibx-9h%e629_e=dm^nbyo6at9lluv=$n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
